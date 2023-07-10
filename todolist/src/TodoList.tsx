f39e85190cda4849881480107bc99aa9417102de
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-07-10 17:02:48 +0900

    added react-hook-form

--- todolist/src/TodoList.tsx
+++ todolist/src/TodoList.tsx
@@ -1,32 +1,56 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function TodoList() {
+//     const [todo, setTodo] = useState("");
+//     const [todoError, setTodoError] = useState("");
+
+//     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//         const {
+//             currentTarget: { value },
+//         } = event;
+//         setTodo(value);
+//     };
+
+//     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+
+//         if (todo.length < 10) {
+//             setTodoError("todo is have to be longer than 10");
+//         }
+//         console.log("submit");
+//     };
+
+//     return (
+//         <div>
+//             <form onSubmit={onSubmit}>
+//                 <input
+//                     onChange={onChange}
+//                     value={todo}
+//                     placeholder="Let me know what you gonna do."
+//                 ></input>
+//                 <button>Add</button>
+//                 {todoError !== "" ? todoError : null}
+//             </form>
+//         </div>
+//     );
+// }
 
 function TodoList() {
-  const [todo, setTodo] = useState("");
+    const { register, watch } = useForm();
+    console.log(watch());
 
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo);
-  };
-
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          placeholder="Let me know what you gonna do."
-        ></input>
-        <button>Add</button>
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <form>
+                <input
+                    {...register("todo")}
+                    placeholder="Let me know what you gonna do."
+                ></input>
+                <button>Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default TodoList;
